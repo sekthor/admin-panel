0b01ff916b3a3729b49ace2f356fbdb39433666a
sekthor <sekthor@protonmail.ch>
2021-12-08 22:21:10 +0100

    styled header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { JwtService } from 'src/app/services/jwt.service';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+
+  loggedIn: boolean = false;
+  username: string = "";
+
+  constructor(
+    private loginService: LoginService,
+    private jwtService: JwtService
+  ) { }
 
   ngOnInit(): void {
+   this.getUsername() 
   }
+
+  getUsername() {
+    if (this.loginService.isLoggedIn())
+      this.username = this.jwtService.getUsername();
+  }
+
+  
 
 }
